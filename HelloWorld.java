a5e81d7078e5eb47143270793ffa8afdadb8700e
venkatramgopal <108490684+venkatramgopal@users.noreply.github.com>
2022-07-01 00:59:50 +0530

    Create HelloWorld.java
    Adding a java file

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld{
+  
+  public static void main(String args[]){
+   
+    System.out.println("Welcome to my Youtube Chaneel");
+    
+    System.out.println("Like & Share");
+  }
+}
